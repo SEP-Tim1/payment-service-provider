ac2df048f5e9408fe7b60e851ed6157afaacd282
Tamara Ranković <rankovictamaraa@gmail.com>
2021-12-08 14:53:27 +0100

    script for starting all services

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+start java -jar registry/target/registry-0.0.1-SNAPSHOT.jar
+start java -jar gateway/target/gateway-0.0.1-SNAPSHOT.jar
+start java -jar auth-service/target/auth-0.0.1-SNAPSHOT.jar
+start java -jar store-service/target/store-0.0.1-SNAPSHOT.jar
+start java -jar card-payment-service/target/payment.card-0.0.1-SNAPSHOT.jar
+start java -jar card-payment-service/target/payment.card-0.0.1-SNAPSHOT.jar --server.port=8123 --spring.application.name=card-payment-service
+start java -jar payment-request-service/target/request-0.0.1-SNAPSHOT.jar
