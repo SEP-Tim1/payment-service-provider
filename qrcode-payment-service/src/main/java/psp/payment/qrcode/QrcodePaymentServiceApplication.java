2223495753da2b4a1fe6d1752a92713f08544cfd
Tamara Ranković <rankovictamaraa@gmail.com>
2022-01-15 13:01:11 +0100

    qr code payment service created

---
+++ qrcode-payment-service/src/main/java/psp/payment/qrcode/QrcodePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package psp.payment.qrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodePaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodePaymentServiceApplication.class, args);
+	}
+
+}
---
+++ qrcode-payment-service/src/test/java/psp/payment/qrcode/QrcodePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package psp.payment.qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodePaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
